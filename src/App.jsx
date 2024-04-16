b846999624e69caccefda34579b43dedc26534be
praduman <praduman03k@gmail.com>
2024-04-16 15:10:36 +0530

    add EQ section content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import HomePageImg from "./assets/home_page_image.png";
+import EQSection from "./components/eq_section/EQSection";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <br />
+      <img className="w-11/12 ml-auto mr-auto" src={HomePageImg} alt="" />
+      <br />
+      <EQSection />
     </div>
   );
 }
---
+++ src/components/eq_section/EQSection.jsx
@@ -0,0 +1,17 @@
+const EQSection = () => {
+  return (
+    <div className="flex justify-around w-11/12 ml-auto mr-auto mt-14 mb-14">
+      <h1 className="text-5xl w-2/5 p-11">EQ beats IQ</h1>
+      <p className="w-1/3 text-xl p-11 leading-10">
+        People with high emotional intelligence (EQ) live more fulfilled lives.
+        They tend to be happier and have healthier relationships{" "}
+      </p>
+      <p className="w-1/3 text-xl p-11 leading-10">
+        They are more successful in their pursuits and make for inspiring
+        leaders. According to science, they earn $29k a year
+      </p>
+    </div>
+  );
+};
+
+export default EQSection;
