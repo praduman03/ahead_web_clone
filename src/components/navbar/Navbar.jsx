bd2b7e1ea30281e629613f6ad221422239d459fc
praduman <praduman03k@gmail.com>
2024-04-17 16:17:14 +0530

    add admin button in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const navigate = useNavigate( )
   return (
     <div className="flex justify-between items-center m-6">
       <img className="w-20" src="/logo.jpg" alt="" />
       <div className="flex gap-36 text-lg font-medium">
-        <button>Emotions</button>
+        <button onClick={() => navigate('/admin')}>Admin</button>
         <button>Manifesto</button>
         <button>Self-awareness test</button>
         <button>Work with Us</button>
